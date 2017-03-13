82a2b0e252ca9b476b292c7b2e414fd16da034fd
root <logicmoo@gmail.com>
2017-03-13 06:54:09 -0700

    v1.1.113

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(slack_prolog).
-version('1.1.112').
+version('1.1.113').
 title('Prolog interface to Slack http://www.slack.com').
 keywords([chat,bots,slack]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
